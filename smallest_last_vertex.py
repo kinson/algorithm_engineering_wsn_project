c60f689b12042f99467b7f68818ed142eaca32e6
Samuel Hunter <swilkinsonhunter@gmail.com>
2016-10-27 13:29:15 -0500

    Refactored lots of the code to fix a bug with vertices not being removed from the degree list, think part two is a O(n) time now

--- smallest_last_vertex.py
+++ smallest_last_vertex.py
@@ -1,5 +1,3 @@
-import numpy
-import os
 import math
 from random_graph_construction import get_adjacency_list
 
@@ -18,69 +16,65 @@ def generate_buckets_of_vertices(adj_list):
     return (buckets, max_degree)
 
 def find_lowest_degree_vertex(vertex_buckets, max_bucket):
-    for i in range(max_bucket):
+    """Return the next lowest degree vertex from the vertex buckets along with the buckets without the removed vertex."""
+    for i in range(max_bucket + 1):
         if i in vertex_buckets and len(vertex_buckets[i]):
             popped_vertex = vertex_buckets[i].pop(0)
             return (popped_vertex, vertex_buckets)
 
-def get_neighbors_and_degrees(vertex, adj_list, aux_dict):
-    """Return a list of neighbors for a given vertex with their degree once the vertex is gone (degree - 1)."""
-    vertex_data = adj_list[vertex]
-    neighbors = {}
+def get_neighbors_and_bump_degrees(vertex_data, aux_dict, buckets):
+    """Iterate over the neighbors to the vertex being removed. Bump their degrees down if they are greater than 0.
+    Shift the neighbors down in the vertices bucket if they exist, otherwise they have already been removed, so continue
+    to next neighbor. Return the buckets and auxillary degree dictionary back."""
+    new_bucket = -1
     for neighbor in vertex_data['connected_points']:
-        if neighbor in aux_dict:
-            neighbors[neighbor] = aux_dict[neighbor] - 1
-        else:
-            neighbors[neighbor] = adj_list[neighbor]['degree'] - 1
-        aux_dict[neighbor] = neighbors[neighbor] #update aux_dict
-    return (neighbors, aux_dict)
+        new_bucket = aux_dict[neighbor] - 1 if aux_dict[neighbor] > 0 else 0
+        aux_dict[neighbor] = new_bucket
 
-def shift_neighbors_down_bucket(buckets, neighbors):
-    """Return buckets with members of neighbors shifted down by one."""
-    for neighbor, bucket in neighbors.items():
-        if neighbor in buckets[bucket + 1]:
-            buckets[bucket + 1].remove(neighbor)
-        if bucket in buckets:
-            buckets[bucket].append(neighbor)
+        if new_bucket + 1 in neighbor and neighbor in buckets[new_bucket + 1]:
+            buckets[new_bucket + 1].remove(neighbor)
         else:
-            buckets[bucket] = [neighbor]
-    return buckets
+            continue
+
+        if new_bucket in buckets:
+            buckets[new_bucket].append(neighbor)
+        else:
+            buckets[new_bucket] = [neighbor]
+
+    return (buckets, aux_dict)
+
+def populate_aux_dict(adj_list):
+    """Return an auxillary dictionary to keep track of the degree of each vertex."""
+    aux_dict = {}
+    for a, b in adj_list.items():
+        d = b['degree']
+        aux_dict[a] = d
+    return aux_dict
 
 def get_smallest_vertex_ordering(adj_list):
     """Return a list of vertices in ascending order of their degrees."""
     vertex_buckets, max_bucket = generate_buckets_of_vertices(adj_list)
     vertices_in_order_of_degree = []
-    aux_vertex_degrees = {}
+    aux_vertex_degrees = populate_aux_dict(adj_list)
+    total_len = len(adj_list)
     for i in range(len(adj_list)):
         #get the vertex with the minimum degree
         min_vert, vertex_buckets = find_lowest_degree_vertex(vertex_buckets, max_bucket)
         #print (min_vert, vertex_buckets)
         vertices_in_order_of_degree.append(min_vert)
         #get all neighbors (and their degrees) of min vertex
-        neighbors, aux_vertex_degrees = get_neighbors_and_degrees(min_vert, adj_list, aux_vertex_degrees)
-
-        #bump all neighbors down by a degree in buckets
-        vertex_buckets = shift_neighbors_down_bucket(vertex_buckets, neighbors)
-        # print(min_vert)
-        # print(neighbors)
-        # print(vertex_buckets)
-        # print()
+        vertex_data = adj_list[min_vert]
+        vertex_buckets, aux_vertex_degrees = get_neighbors_and_bump_degrees(vertex_data, aux_vertex_degrees, vertex_buckets)
     return vertices_in_order_of_degree
 
 
 #HAVEN'T IMPLEMENTED YET
-
 def color_vertices(smallest_first_vertices):
     """Return the list of vertices with colors associated with them."""
     colors = [1]
     for v in smallest_first_vertices:
         pass
 
-
-
 if __name__ == '__main__':
-
-    a_list = get_adjacency_list(10, 3, 'square')
-    print(a_list)
+    a_list = get_adjacency_list(64000, 64, 'square')
     smallest_ordered_vertices = get_smallest_vertex_ordering(a_list)
-    print(smallest_ordered_vertices)
