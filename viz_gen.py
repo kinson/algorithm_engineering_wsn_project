67c71366c5be078ee36b8d40fece8abe73e9bf35
Samuel Hunter <swilkinsonhunter@gmail.com>
2016-11-20 18:21:30 -0600

    beginning visualizations

---
+++ viz_gen.py
@@ -0,0 +1,9 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+
+if __name__ == "__main__":
+  print("starting part one generation")
+
+  #for each benchmark, generate the following graphic
